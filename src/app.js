6438302f3eff5f41a420c1bd30364b069f295149
Nayeon Shim <nshim1@my.centennialcollege.ca>
2022-10-15 12:36:23 -0400

    server

--- src/app.js
+++ src/app.js
@@ -1,10 +1,28 @@
 const express = require("express"); //외부에서 설치된파일을 import
+const morgan = require("morgan");
+const path = require("path");
 
 const app = express();
+app.use(morgan("combined"));
+console.log(path.join(__dirname, "public"));
+app.use(express.static(path.join(__dirname, "public"))); //join +
+//static <- be able to access through address with file name
 
 //Read
 app.get("/", (req, res) => {
-  res.send("Welcome to server");
+  // const name = req.query.name ?? "";
+  const { name, age } = req.query ?? "";
+  res.send(`Welcome to server,${name}`);
+});
+app.get("/:name", (req, res) => {
+  const { name } = req.params;
+  res.send(`Welcome to server${name}`);
+});
+app.get("/home", (req, res) => {
+  res.redirect("/");
+});
+app.get("/about", (req, res) => {
+  res.send("about");
 });
 
 app.listen(8080, () => {
