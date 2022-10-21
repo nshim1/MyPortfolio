4a933402990a78f7a02d83b069ddbb3a5fec87ee
Nayeon Shim <nshim1@my.centennialcollege.ca>
2022-10-21 16:14:36 -0400

    applied ejs template

--- src/app.js
+++ src/app.js
@@ -1,30 +1,38 @@
-const express = require("express"); //외부에서 설치된파일을 import
-const morgan = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
 const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
+const indexRouter = require("./routes/index");
 const app = express();
-app.use(morgan("combined"));
-console.log(path.join(__dirname, "public"));
-app.use(express.static(path.join(__dirname, "public"))); //join +
-//static <- be able to access through address with file name
 
-//Read
-app.get("/", (req, res) => {
-  // const name = req.query.name ?? "";
-  const { name, age } = req.query ?? "";
-  res.send(`Welcome to server,${name}`);
-});
-app.get("/:name", (req, res) => {
-  const { name } = req.params;
-  res.send(`Welcome to server${name}`);
-});
-app.get("/home", (req, res) => {
-  res.redirect("/");
-});
-app.get("/about", (req, res) => {
-  res.send("about");
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
-app.listen(8080, () => {
-  console.log("Your server is listening on 8080.");
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
+
+module.exports = app;
---
+++ src/routes/index.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  const userInfo = {
+    name: "Nayeon Shim",
+    image: "me.png",
+  };
+  res.render("index", { userInfo });
+});
+/* GET contact page. */
+router.get("/contact", function (req, res, next) {
+  const userInfo = {
+    name: "Nayeon Shim",
+    email: "nayeon.shim@gmail.com",
+    phone: "1234567890",
+    title: "Web Designer & Developer",
+    address: "777 Bay St, Toronto, ON, Candada",
+  };
+  res.render("contact", { userInfo });
+});
+
+router.post("/messages", function (req, res, next) {
+  res.redirect("/");
+});
+
+module.exports = router;
