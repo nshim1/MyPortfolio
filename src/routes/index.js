b6dca243eb109a9d569864dd4d6d6d2a2dcd0058
Nayeon Shim <nshim1@my.centennialcollege.ca>
2022-11-02 22:11:20 -0400

    login view

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,18 @@
 var express = require("express");
+const passport = require("passport");
+const User = require("../models/user");
 var router = express.Router();
 
 function requireAuth(req, res, next) {
   // check if the user is logged in
 
   // ADD YOUR CODE HERE
+  console.log("auth status:", req.isAuthenticated());
   if (req.isAuthenticated()) {
     //req.isAuthenticated() will return true if user is logged in
     next();
   } else {
-    res.redirect("/views/signin");
+    res.redirect("/signin");
   }
 }
 
@@ -59,6 +62,16 @@ router.get("/signin", function (req, res, next) {
     isAuth: req.isAuthenticated(),
   });
 });
+
+router.post("/signin", function (req, res, next) {
+  //passport local authentication
+  passport.authenticate("local", {
+    successRedirect: req.session.url || "/",
+    failureRedirect: "/signup",
+    failureFlash: true,
+  })(req, res, next); // curling
+  delete req.session.url;
+});
 router.get("/signup", function (req, res, next) {
   if (req.isAuthenticated()) {
     res.redirect("/");
@@ -67,6 +80,48 @@ router.get("/signup", function (req, res, next) {
     title: "Sign up",
     isAuth: req.isAuthenticated(),
     user: {},
+  });
+});
+
+router.post("/signup", function (req, res, next) {
+  if (!req.user && req.body.password === req.body.password_confirm) {
+    console.log(req.body);
+
+    let user = new User(req.body);
+    user.provider = "local";
+    console.log(user);
+
+    user.save((err) => {
+      if (err) {
+        return res.render("/signup", {
+          title: "Sign-up Form",
+          messages: req.flash("error"),
+          user: user,
+        });
+      }
+      req.login(user, (err) => {
+        if (err) return next(err);
+        return res.redirect("/");
+      });
+    });
+  } else {
+    return res.redirect("/");
+  }
+});
+
+router.get("/signout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+router.get("/business", requireAuth, function (req, res, next) {
+  res.render("business", {
+    title: "Business Contact List",
+    isAuth: req.isAuthenticated(),
   });
 });
 
